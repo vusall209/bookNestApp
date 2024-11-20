be3a96673ccb91aefc199b1ae6df3ed89db5d60f
vusall209 <127177830+vusall209@users.noreply.github.com>
2024-11-20 12:38:55 +0400

    Commit

--- src/main/java/az/booknestapp/repository/MyBookRepository.java
+++
@@ -1,9 +0,0 @@
-package az.booknestapp.repository;
-
-import az.booknestapp.entity.MyBookList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyBookRepository extends JpaRepository<MyBookList,Integer> {
-}
--- src/main/java/az/booknestapp/service/MyBookListServiceImpl.java
+++ src/main/java/az/booknestapp/service/MyBookListServiceImpl.java
@@ -1,16 +1,11 @@
 package az.booknestapp.service;
 
 import az.booknestapp.entity.MyBookList;
-import az.booknestapp.repository.MyBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MyBookListServiceImpl {
-    private final MyBookRepository myBookRepository;
 
-    public void saveMyBooks(MyBookList book){
-        myBookRepository.save(book);
-    }
 }
